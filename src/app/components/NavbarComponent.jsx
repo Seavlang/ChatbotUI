6e05ba8a8cd5ec81d0d2af671520451348ed2866
veyseavlang <veyseavlang@veyseavlangs-MacBook-Pro.local>
2024-09-26 23:59:15 +0700

    navbar

---
+++ src/app/components/NavbarComponent.jsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function NavbarComponent() {
+  return (
+    <div>
+       <div className="navbar bg-base-100 font-semibold text-[#004B93]">
+        <div className="navbar-start">
+            <div>
+            <Image 
+                src="/asset/images/logo.png" 
+                alt="GPT_LOGO" 
+                width={40} 
+                height={40}
+            />
+            </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal text-lg">
+            <li>
+                <Link href="/playground">
+                Playground
+                </Link>
+            </li>
+            <li>
+                <Link href="/docs">
+                Docs
+                </Link>
+            </li>
+            <li>
+                <Link href="/examples">
+                Examples
+                </Link>
+            </li>
+            <li>
+                <Link href="/about">
+                About
+                </Link>
+            </li>
+        </ul>
+        </div>
+        <div className="navbar-end text-2xl">
+            <a className="">Login</a>
+        </div>
+       </div>
+    </div>
+  );
+}
