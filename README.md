03352ded92acab46ecc946f5b9fc813c514b225e
veyseavlang <veyseavlang@veyseavlangs-MacBook-Pro.local>
2024-11-12 11:55:38 +0700

    loading & ui update

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ bun dev
 ## How to Use
 
 ```javascript
-import  Chatbot  from "@seavlang/hrdchatbotwidget/src/index";
+import  Chatbot  from "@kshrd/chatbotwidget/src/index";
 
 export default function Chat() {
   return 
@@ -29,9 +29,22 @@ export default function Chat() {
   </div>;
 }
 
-
+#Testing Project 
+In this example I have uploaded the file that contains the final project of 11th Gen Student, which includes groups (LMS, EasyCart,FinTrack,TradeWise,AutoPilot,e-certify,DataVue)
 
 You can test the package with this: 
-  session id =  "13", 
+  session id =  "14", 
   project id =  "8", 
   api key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X25hbWUiOiJzZWF2bGFuZyIsImVtYWlsIjoic2lldmxhbmd2ZXlAZ21haWwuY29tIn0.BYKAF4dQl34kppfrH_SS29ef4se5Qpr3cQ-1iNaolX0"
+
+#Suggest Questions: 
+-What problem does the AutoPilot platform aim to solve for developers?
+-How does LMS support multiple languages for leave management?
+-What is the primary purpose of the easycart mobile application?
+-How does FinTrack assist bank clients with transaction notifications?
+-What technology does e-Certify use to ensure certificate authenticity and security?
+-What key feature does TradeWise offer to connect buyers and sellers?
+-What types of notifications can FinTrack send to its users?
+-How does DataVue provide financial information to users?
+-In what ways does the AutoPilot platform support CI/CD for developers?
+-How does LMS streamline the leave request process for employees?
--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,7 @@ export default function Chat({ apiKey, SessionId, projectId }) {
     { type: 'bot', text: "Hello! I'm here to lend a helping hand whenever you need it. What can I do for you?" },
   ]);
   const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const chatRef = useRef(null);
 
   useEffect(() => {
@@ -25,14 +26,18 @@ export default function Chat({ apiKey, SessionId, projectId }) {
     if (input.trim() === '') return;
 
     setMessages((prevMessages) => [...prevMessages, { type: 'user', text: input }]);
+    setInput(''); // Clear input after sending
+    setIsLoading(true); // Start loading
+
     try {
-      const response = await ChatbotAction(input, SessionId , projectId , apiKey);
+      const response = await ChatbotAction(input, "14", "8", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X25hbWUiOiJzZWF2bGFuZyIsImVtYWlsIjoic2lldmxhbmd2ZXlAZ21haWwuY29tIn0.BYKAF4dQl34kppfrH_SS29ef4se5Qpr3cQ-1iNaolX0");
       setMessages((prevMessages) => [...prevMessages, { type: 'bot', text: response.output }]);
     } catch (error) {
       setMessages((prevMessages) => [...prevMessages, { type: 'bot', text: 'Sorry, there was an error processing your request.' }]);
       console.error('Error:', error);
+    } finally {
+      setIsLoading(false); // Stop loading
     }
-    setInput('');
   };
 
   const handleKeyDown = (e) => {
@@ -46,8 +51,8 @@ export default function Chat({ apiKey, SessionId, projectId }) {
     <div
       style={{
         position: 'fixed',
-        bottom: '10px',
-        right: '10px',
+        bottom: '30px',
+        right: '30px',
         boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
         transition: 'transform 0.3s ease',
         width: isExpanded ? '450px' : '80px',
@@ -60,57 +65,40 @@ export default function Chat({ apiKey, SessionId, projectId }) {
     >
       {isExpanded ? (
         <div style={{ display: 'flex', flexDirection: 'column', height: '100%', border: '1px solid white', borderRadius: '24px', boxShadow: '0 -4px 10px rgba(0, 0, 0, 0.2), 0 4px 10px rgba(0, 0, 0, 0.1)' }}>
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px', backgroundColor: 'white', color: '#004B93', borderTopLeftRadius: '16px', borderTopRightRadius: '16px' }}>
-            <span style={{ fontWeight: 'bold' }}>AI Assistant</span>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px', backgroundColor: 'white', color: '#004B93', borderTopLeftRadius: '16px', borderTopRightRadius: '16px', borderBottom:'1px solid #004B93' }}>
+            <span style={{ fontWeight: 'bold', }}>AI Assistant</span>
             <button onClick={() => setIsExpanded(false)} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
               <Image src={closeIcon} width={30} height={30} alt="close" />
             </button>
           </div>
+          {/* <hr style={{border: '1px solid gray'}}/> */}
           <ul ref={chatRef} style={{ flex: 1, overflowY: 'auto', padding: '16px', backgroundColor: '#ebf4ff', margin: 0, listStyle: 'none' }}>
             {messages.map((message, index) => (
-             <ul key={index} style={{width: "100%", display: "flex",marginBottom: '8px', justifyContent: message.type === "user" ? "end" : "start"}}>
-              {message.type === 'user' ? <li
-                
-                style={{
-                  padding: '12px',
-                  fontSize: '14px',
-                  backgroundColor: '#004B93',
-                  color: 'white',
-                  borderRadius: '16px 16px 0px 16px',
-                  // float: message.type === 'user' ? 'right' : 'left', // User messages float right, bot messages float left
-                  maxWidth: '80%',
-                  marginBottom: '8px',
-                  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                  textAlign: 'right', // Align text based on message type
-                  wordBreak: 'break-word' ,
-                  overflowWrap: 'break-word'
-                  
-                }}
-              >
-                {message.text}
-              </li> : <li
-                style={{
-                  padding: '12px',
-                  fontSize: '14px',
-                  backgroundColor: 'white',
-                  color: '#4a4a4a',
-                  borderRadius: '16px 16px 16px 0px',
-                  // float: message.type === 'user' ? 'right' : 'left', // User messages float right, bot messages float left
-                  maxWidth: '80%',
-                  marginBottom: '8px',
-                  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                  textAlign: 'left' ,// Align text based on message type
-                  wordBreak: 'break-word' ,
-                  overflowWrap: 'break-word'
-                }}
-              >
-                {message.text}
-              </li>}
-               
-             </ul>
+              <ul key={index} style={{ width: "100%", display: "flex", marginBottom: '8px', justifyContent: message.type === "user" ? "end" : "start" }}>
+                <li
+                  style={{
+                    padding: '12px',
+                    fontSize: '14px',
+                    backgroundColor: message.type === "user" ? '#004B93' : 'white',
+                    color: message.type === "user" ? 'white' : '#4a4a4a',
+                    borderRadius: message.type === "user" ? '16px 16px 0px 16px' : '16px 16px 16px 0px',
+                    maxWidth: '80%',
+                    textAlign: 'left',
+                    wordBreak: 'break-word',
+                    overflowWrap: 'break-word'
+                  }}
+                >
+                  {message.text}
+                </li>
+              </ul>
             ))}
+            {isLoading && (
+              <div style={{ display: 'flex', justifyContent: 'start', padding: '12px', fontSize: '14px', backgroundColor: 'white', color: '#4a4a4a', borderRadius: '16px 16px 16px 0px', maxWidth: '80%', marginBottom: '8px'}}>
+                Typing...
+              </div>
+            )}
           </ul>
-          <div style={{ display: 'flex', alignItems: 'center', padding: '12px', borderTop: '1px solid #cbd5e0', backgroundColor: 'white', borderBottomLeftRadius: '16px', borderBottomRightRadius: '16px' }}>
+          <div style={{ display: 'flex', alignItems: 'center', padding: '12px', borderTop: '1px solid #004B93', backgroundColor: 'white', borderBottomLeftRadius: '16px', borderBottomRightRadius: '16px' }}>
             <input
               type="text"
               value={input}
@@ -125,11 +113,9 @@ export default function Chat({ apiKey, SessionId, projectId }) {
                 borderRadius: '12px',
                 marginRight: '8px'
               }}
+              disabled={isLoading} // Disable input while loading
             />
-            {/* <button onClick={handleSend} style={{ fontSize: '16px', color: '#888', background: 'none', border: 'none', cursor: 'pointer' }}>
-              ➤
-            </button> */}
-            <Image onClick={handleSend} src={sent} width={30} height={30} alt="close" style={{ cursor: 'pointer' }}/>
+            <Image onClick={handleSend} src={sent} width={30} height={30} alt="send" style={{ cursor: 'pointer' }} />
           </div>
         </div>
       ) : (
