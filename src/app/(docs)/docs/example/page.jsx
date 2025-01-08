8ebcc467205753dfbe218bc30266e6604f5c70c3
veyseavlang <sievlangvey@gmail.com>
2025-01-08 11:42:34 +0700

    Responsive Example

--- src/app/(docs)/docs/example/page.jsx
+++ src/app/(docs)/docs/example/page.jsx
@@ -31,16 +31,16 @@ export default function Page() {
     <div className="flex justify-center">
       <div className="w-4/5">
         <div className="min-h-screen bg-white dark:bg-gray-900 dark:text-gray-300">
-          <h1 className="mx-10 mt-10 mb-5 text-4xl font-medium text-primary dark:text-white">
+          <h1 className="mx-10 mt-10 md:mb-5 mb-3 text-xl md:text-4xl  font-medium text-primary dark:text-white">
             Example
           </h1>
-          <p className="ml-10 mr-20 mb-5 dark:text-gray-400">
+          <p className="ml-10 mr-20 mb-5 text-sm md:text-lg dark:text-gray-400">
             First up, let&apos;s learn how to use a language model by itself.
             LangChain supports many different language models that you can use
             interchangeably - select the one you want to use below!
           </p>
 
-          <div className="mx-10 rounded-t-md bg-[#004655] dark:bg-gray-700 text-white border-b-[1px] border-white px-4 py-2 flex justify-between items-center">
+          <div className="mx-10 w-[75%] rounded-t-md bg-[#004655] dark:bg-gray-700 text-white border-b-[1px] border-white px-4 py-2 flex justify-between items-center">
             <span className="font-semibold">Code</span>
 
             {/* Custom Copy Button */}
@@ -57,7 +57,7 @@ export default function Page() {
               )}
             </button>
           </div>
-          <div className="mx-10 border-b-2 border-x-2 mb-10 rounded-b-md bg-white dark:bg-gray-800 dark:border-gray-700">
+          <div className="mx-10 border-b-2  w-[75%] text-sm md:text-lg border-x-2 mb-10 rounded-b-md bg-white dark:bg-gray-800 dark:border-gray-700">
             <CodeBlock
               text={codeString}
               language="html"
